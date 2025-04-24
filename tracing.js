dd37cb3a41292b6d9e0736e526c1c1f14e0ebdf4
eStrau <ellestrauss01@gmail.com>
2025-04-23 22:21:43 -0400

    finishing code additions and importing needed libraries

--- tracing.js
+++ tracing.js
@@ -1,37 +1,33 @@
-const { Resource } = require("@opentelemetry/resources");
-const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { trace } = require("@opentelemetry/api");
+const { resourceFromAttributes } = require('@opentelemetry/resources');
+const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations, } = require('@opentelemetry/auto-instrumentations-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const { ConsoleMetricExporter, PeriodicExportingMetricReader, } = require('@opentelemetry/sdk-metrics');
 
-const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+//Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
-module.exports = (serviceName) => {
-  const exporter = new JaegerExporter({
-    endpoint: "http://localhost:14268/api/traces",
-  });
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { OTLPTraceExporter, } = require('@opentelemetry/exporter-trace-otlp-proto');
+const { OTLPMetricExporter,} = require('@opentelemetry/exporter-metrics-otlp-proto');
 
-  const provider = new NodeTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-    }),
-  });
 
-  provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-  provider.register();
+const sdk = new NodeSDK({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: "todo-service"
+  }),
+  traceExporter: new OTLPTraceExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation()],
+});
 
-  registerInstrumentations({
-    instrumentations: [
-      new HttpInstrumentation(),
-      new ExpressInstrumentation(),
-      new MongoDBInstrumentation(),
-    ],
-    tracerProvider: provider,
-  });
-
-  return trace.getTracer(serviceName);
-};
+sdk.start();
